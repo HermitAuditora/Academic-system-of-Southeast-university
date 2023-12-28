e464c3533e451f440f5ce0b2716f9d23f2c44f1b
HermitAuditora <82577403+HermitAuditora@users.noreply.github.com>
2023-12-28 21:35:59 +0800

    Add files via upload

---
+++ webpage/Course.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Course.aspx.cs" Inherits="AcademicSystem.webpage.Course" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="icon" href="../mainboardimg/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="../mainboardimg/favicon1.ico" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="../maincss/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="../maincss/error.css" />
+<link rel="stylesheet" type="text/css" href="../maincss/zftal-ui.css" />
+<link rel="stylesheet" type="text/css" href="../maincss/maincss.css" />
+
+
+<title>学生课表查询</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+	<!--最上方logo -->
+	<header class="navbar-inverse top2" id="show-head">
+		<div class="container" id="navbar_container">
+					<div class="container">
+			<div class="navbar-header">
+				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+					<span class="sr-only"> 学生课表查询</span> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span>
+				</button>
+				<asp:Label runat="server" CssClass="navbar-brand">学生课表查询</asp:Label>
+			</div>
+		</div>
+		</div>
+	</header>
+
+
+	<div style="width: 100%; padding: 0px; margin: 0px;" id="bodyContainer">
+		<div class="container container-func sl_all_bg" id="yhgnPage">
+			<div id="innerContainer">
+	<!--学年 学期-->
+	<div class="row">
+	<div class="col-md-4 col-sm-5">
+		<div class="form-group">
+			<label for="" class="col-sm-2 control-label" id="xn"><span style="color:red;">*</span>学年</label>
+			<div class="col-sm-8">
+				<asp:DropDownList ID="Year" runat="server" CssClass="form-control">
+					    <asp:ListItem Text="2023-2024" Value="2023-2024"></asp:ListItem>
+						<asp:ListItem Text="2022-2023" Value="2022-2023" Selected="True"></asp:ListItem>
+						<asp:ListItem Text="2021-2022" Value="2021-2022"></asp:ListItem>
+						<asp:ListItem Text="2020-2021" Value="2020-2021"></asp:ListItem>
+				</asp:DropDownList>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4 col-sm-5">
+		<div class="form-group">
+			<label for="" class="col-sm-2 control-label" id="xq"><span style="color:red;">*</span>学期</label>
+			<div class="col-sm-8">
+				<asp:DropDownList ID="Term" runat="server" CssClass="form-control chosen-select">
+						<asp:ListItem Text="1" Value="1"></asp:ListItem>
+						<asp:ListItem Text="2" Value="2" Selected="True"></asp:ListItem>
+				</asp:DropDownList>
+			</div>
+		</div>
+	</div>
+<div class="col-md-12 col-sm-12"><div class="form-group"><div class="col-sm-12">
+		<asp:Button ID="Query" runat="server" CssClass="btn btn-primary btn-sm pull-right bigger-120 glyphicon glyphicon-search" Text="查询" OnClick="Query_Click" />
+	</div></div></div>
+			</div>
+
+
+
+
+		</div>
+		</div>
+		</div>
+	<div style="width: 100%; padding: 0px; margin: 0px;" id="bodyContainer">
+		<div class="container container-func sl_all_bg" id="yhgnPage">
+			<div id="innerContainer">
+				<!--课表-->
+			<asp:GridView ID="GridView1" runat="server"   CssClass="gridview-style" Height="171px" OnRowDataBound="GridView1_RowDataBound">
+
+            <Columns>
+                <asp:BoundField HeaderText="时间段" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="节次" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="星期一" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="星期二" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="星期三" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="星期四" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="星期五" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="星期六" HeaderStyle-CssClass="gridview-header"/>
+                <asp:BoundField HeaderText="星期日" HeaderStyle-CssClass="gridview-header"/>
+            </Columns>
+        </asp:GridView>
+				</div>
+			</div>
+		</div>
+
+    </form>
+</body>
+</html>
---
+++ webpage/Course.aspx.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AcademicSystem.App_Code;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AcademicSystem.webpage
+{
+    public partial class Course : System.Web.UI.Page
+    {
+        private string username;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            username = Request.QueryString["username"];
+            if (!IsPostBack)
+            {
+
+                DataTable table = new DataTable();
+                for (int i = 0; i < 14; i++)
+                {
+                    DataRow row = table.NewRow();
+                    table.Rows.Add(row);
+                }
+
+                GridView1.AutoGenerateColumns = false;
+                GridView1.DataSource = table;
+                GridView1.DataBind();
+            }
+
+
+        }
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                //第一列合并(1-4、5-6、7-11、12-14)
+                if (e.Row.RowIndex < 4)
+                {
+                    if (e.Row.RowIndex == 0)
+                    {
+                        e.Row.Cells[0].RowSpan = 4; // 合并4行
+                        e.Row.Cells[0].HorizontalAlign = HorizontalAlign.Center;
+                        e.Row.Cells[0].Text = "上午";
+
+                    }
+
+                    else
+                        e.Row.Cells[0].Visible = false; // 隐藏其他行的第一列
+                }
+                else if (e.Row.RowIndex >= 4 && e.Row.RowIndex < 6)
+                {
+                    if (e.Row.RowIndex == 4)
+                    {
+                        e.Row.Cells[0].RowSpan = 2;
+                        e.Row.Cells[0].HorizontalAlign = HorizontalAlign.Center;
+                        e.Row.Cells[0].Text = "中午";
+                    }
+
+                    else
+                        e.Row.Cells[0].Visible = false; // 隐藏其他行的第一列
+                }
+                else if (e.Row.RowIndex >= 6 && e.Row.RowIndex < 11)
+                {
+                    if (e.Row.RowIndex == 6)
+                    {
+                        e.Row.Cells[0].RowSpan = 5;
+                        e.Row.Cells[0].HorizontalAlign = HorizontalAlign.Center;
+                        e.Row.Cells[0].Text = "下午";
+                    }
+                    else
+                        e.Row.Cells[0].Visible = false;
+                }
+                else if (e.Row.RowIndex >= 11 && e.Row.RowIndex < 14)
+                {
+                    if (e.Row.RowIndex == 11)
+                    {
+                        e.Row.Cells[0].RowSpan = 3;
+                        e.Row.Cells[0].HorizontalAlign = HorizontalAlign.Center;
+                        e.Row.Cells[0].Text = "晚上";
+                    }
+
+                    else
+                        e.Row.Cells[0].Visible = false;
+                }
+
+                //填写第二列数据(1-14)
+                e.Row.Cells[1].HorizontalAlign = HorizontalAlign.Center;
+                e.Row.Cells[1].Text = (e.Row.RowIndex + 1).ToString();
+
+                //填写课表数据
+                List<List<string>> results = Takeclass.takecourseinfo(username);
+                List<string> result = Takeclass.processcourse(results);
+                GridView1.Caption = StudentService.findnamebyid(username) + "的课表";
+
+                foreach (List<string> list in results)
+                {
+                    //定位在哪一节课
+                    int row1 = Convert.ToInt32(list[4]) - 1;
+                    int row2 = Convert.ToInt32(list[5]) - 1;
+                    int column = Convert.ToInt32(list[6]) + 1;
+                    if (list[16].Equals(Year.SelectedValue)&&list[17].Equals(Term.SelectedValue))
+                    if (e.Row.RowIndex >= row1 && e.Row.RowIndex <= row2)
+                    {
+                        if (e.Row.RowIndex == row1)
+                        {
+                            e.Row.Cells[column].RowSpan = row2 - row1 + 1;
+                            e.Row.Cells[column].Text += result[Convert.ToInt32(list[list.Count-1])];
+                            e.Row.Cells[column].CssClass = "course_style";
+                        }
+                        else
+                            e.Row.Cells[column].Visible = false;
+                    }
+
+                }
+            }
+
+        }
+
+        protected void Query_Click(object sender, EventArgs e)
+        {
+            //点击按钮根据学期学年更新课表
+            DataTable table = new DataTable();
+            for (int i = 0; i < 14; i++)
+            {
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+            }
+
+            GridView1.AutoGenerateColumns = false;
+            GridView1.DataSource = table;
+            DataBind();
+        }
+    }
+}
